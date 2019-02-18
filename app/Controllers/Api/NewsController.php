606073489b5c0a268f20ea6c916e15b161c38e4b
kevin0217 <kevin0217@126.com>
2019-02-18 14:26:21 +0800

    RESTFUL API News添加修改删除
    命令生成MODEL
    php bin/swoft entity:create -d test prod

--- app/Controllers/Api/NewsController.php
+++ app/Controllers/Api/NewsController.php
@@ -3,21 +3,90 @@ namespace App\Controllers\Api;
 
 use App\Models\Entity\News;
 use Swoft\Db\Query;
+use Swoft\Http\Message\Server\Request;
 use Swoft\Http\Server\Bean\Annotation\Controller;
 use Swoft\Http\Server\Bean\Annotation\RequestMapping;
 use Swoft\Http\Server\Bean\Annotation\RequestMethod;
 
 /**
- * @Controller("/news")
+ * @Controller("/api/news")
  */
 class NewsController
 {
     /**
-     * @RequestMapping(route="list", method={RequestMethod::GET})
+     * @RequestMapping(route="/api/news", method={RequestMethod::GET})
+     * @param Request $request
+     * @return mixed
      */
-    public function getList()
+    public function getList(Request $request)
     {
-        $result = Query::table(News::class)->orderBy("id","desc")->limit(3)->get()->getResult();
+        $page = $request->input('page',1);
+        $pageSize = $request->input('pageSize',5);
+        $page = empty($page) ? 1 : $page;
+        $pageSize = empty($pageSize) ? 5 : $pageSize;
+        $offset = ($page - 1) * $pageSize;
+        $result = Query::table(News::class)->orderBy("id","desc")->limit($pageSize,$offset)->get()->getResult();
+        return $result;
+    }
+
+    /**
+     * @RequestMapping(route="{id}",method={RequestMethod::GET})
+     * @param Request $request
+     * @param int $id
+     * @return mixed
+     */
+    public function detail(Request $request,int $id)
+    {
+        $get = $request->getQueryParams();
+        $model = News::findOne(['id' =>$id])->getResult();
+        $body['rs'] = $model;
+        $body['get'] = $get;
+        return $body;
+    }
+
+    /**
+     * @RequestMapping(route="/api/news",method={RequestMethod::POST})
+     * @param Request $request
+     * @return mixed
+     */
+    public function create(Request $request)
+    {
+        $name = $request->input('name');
+        $body = $request->getParsedBody();
+        if(empty($name)) return 'name not empty';
+
+        $model = new News();
+        $model->setTitle($name);
+        $id = $model->save()->getResult();
+        $body['id'] = $id;
+        return $body;
+    }
+
+    /**
+     * @RequestMapping(route="{id}",method={RequestMethod::PUT, RequestMethod::PATCH})
+     * @param Request $request
+     * @param int $id
+     * @return mixed
+     */
+    public function update(Request $request,int $id)
+    {
+        $name = $request->input('name');
+
+        $model = News::findById($id)->getResult();
+        $model->setTitle($name);
+        $res = $model->update()->getResult();
+        return $res;
+    }
+
+    /**
+     * @RequestMapping(route="{id}",method={RequestMethod::DELETE})
+     * @param Request $request
+     * @param int $id
+     * @return Request
+     */
+    public function delete(Request $request, int $id)
+    {
+        $result = News::deleteByIds([$id])->getResult();
         return $result;
     }
 
---
+++ app/Models/Entity/Prod.php
@@ -0,0 +1,145 @@
+<?php
+namespace App\Models\Entity;
+
+use Swoft\Db\Model;
+use Swoft\Db\Bean\Annotation\Column;
+use Swoft\Db\Bean\Annotation\Entity;
+use Swoft\Db\Bean\Annotation\Id;
+use Swoft\Db\Bean\Annotation\Required;
+use Swoft\Db\Bean\Annotation\Table;
+use Swoft\Db\Types;
+
+/**
+ * @Entity()
+ * @Table(name="prod")
+ * @uses      Prod
+ */
+class Prod extends Model
+{
+    /**
+     * @var int $id 
+     * @Id()
+     * @Column(name="id", type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string $name 
+     * @Column(name="name", type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @var string $desc 
+     * @Column(name="desc", type="string", length=50)
+     */
+    private $desc;
+
+    /**
+     * @var string $content 
+     * @Column(name="content", type="text", length=65535)
+     */
+    private $content;
+
+    /**
+     * @var int $time 
+     * @Column(name="time", type="integer", default=0)
+     */
+    private $time;
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setId(int $value)
+    {
+        $this->id = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     * @return $this
+     */
+    public function setName(string $value): self
+    {
+        $this->name = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     * @return $this
+     */
+    public function setDesc(string $value): self
+    {
+        $this->desc = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     * @return $this
+     */
+    public function setContent(string $value): self
+    {
+        $this->content = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     * @return $this
+     */
+    public function setTime(int $value): self
+    {
+        $this->time = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+}
