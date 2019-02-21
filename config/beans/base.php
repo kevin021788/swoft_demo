d7416fa66acf68133f0ed52f7b9989c979ebc910
kevin0217 <kevin0217@126.com>
2019-02-21 15:53:47 +0800

    开启中间件， 开发工具 http://192.168.5.181:81/__devtool/http/routes

--- config/beans/base.php
+++ config/beans/base.php
@@ -11,7 +11,7 @@ return [
     'serverDispatcher' => [
         'middlewares' => [
             \Swoft\View\Middleware\ViewMiddleware::class,
-            // \Swoft\Devtool\Middleware\DevToolMiddleware::class,
+             \Swoft\Devtool\Middleware\DevToolMiddleware::class,
             // \Swoft\Session\Middleware\SessionMiddleware::class,
         ]
     ],
--- config/properties/app.php
+++ config/properties/app.php
@@ -27,4 +27,12 @@ return [
     'service'      => require __DIR__ . DS . 'service.php',
     'breaker'      => require __DIR__ . DS . 'breaker.php',
     'provider'     => require __DIR__ . DS . 'provider.php',
+    'devtool' => [
+        // 是否开启 DevTool，默认值为 false
+        'enable' => true,
+        // (可选)前台运行服务器时，是否打印事件调用到 Console
+        'logEventToConsole' => true,
+        // (可选)前台运行服务器时，是否打印 HTTP 请求到 Console
+        'logHttpRequestToConsole' => true,
+    ],
 ];
